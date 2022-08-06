53d932594bad37e53534c4f29503e17dcb30047f
ruchi115 <110588584+ruchi115@users.noreply.github.com>
2022-08-06 16:06:59 +0530

    Delete src directory

--- src/main/java/com/product/repository/SubsprodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.product.entity.subsprod;
-
-public interface SubsprodRepository extends JpaRepository <subsprod , Integer> {
-
-}
